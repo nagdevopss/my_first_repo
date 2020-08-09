eba899d1426b547e4475e8df65c75e3af70155a1
Nagendra <knagendraprasadadobe@gmail.com>
2020-08-09 22:50:34 +0530

    Finnal changes for new features

---
+++ command_args_05.py
@@ -0,0 +1,8 @@
+
+from sys import argv
+user_name = argv[1]
+pwd_01 = argv[2]
+if (user-name == "Nagendra") and (pwd_01 == "Prasad"):
+    print("   ", "Welcome to www.nagendra.com")
+else:
+    print("   ", "Please verify User name and Password")
