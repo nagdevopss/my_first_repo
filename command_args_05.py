8d579097779cf0c556a1bbee44925f4eb76b6d99
Nagendra <knagendraprasadadobe@gmail.com>
2020-08-09 23:10:40 +0530

    New Feature Implemented in master branch

--- command_args_05.py
+++ command_args_05.py
@@ -2,7 +2,11 @@
 from sys import argv
 user_name = argv[1]
 pwd_01 = argv[2]
+list_01 = ["Nagendra", "Prasad"]
 if (user-name == "Nagendra") and (pwd_01 == "Prasad"):
     print("   ", "Welcome to www.nagendra.com")
 else:
     print("   ", "Please verify User name and Password")
+
+for i in list_01:
+    print("  ", i)
